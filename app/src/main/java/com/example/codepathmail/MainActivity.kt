13095470b1155b7c220f5565007e60822eec308d
arlarv <arlonarvesu@gmail.com>
2023-09-21 17:15:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/codepathmail/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.codepathmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var emails: List<Email>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+            val newEmails = EmailFetcher.getNext5Emails()
+            (emails as MutableList<Email>).addAll(newEmails)
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
